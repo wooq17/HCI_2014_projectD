877332a46a6c3ad4435d23165b3b03204f6846be
wooq <wooq@nhnnext.org>
2014-04-24 21:02:14 +0900

    close your bag

--- ARDUINO/closeYourBag/closeYourBag.ino
+++ ARDUINO/closeYourBag/closeYourBag.ino
@@ -1,29 +1,95 @@
-int SENSOR_VIBRATION = A0;
+#include <DueTimer.h>
+
+// gyro
+int ACCELERATION_X = A0;
+int ACCELERATION_Y = A1;
+int ACCELERATION_Z = A2;
+
+int xValuePrev = 0;
+int yValuePrev = 0;
+int zValuePrev = 0;
+
+int xValueCurr = 0;
+int yValueCurr = 0;
+int zValueCurr = 0;
+
+int dx = 0;
+int dy = 0;
+int dz = 0;
+
+// photo interrupter
 int SENSOR_PHOTO = A3;
 
-String message = "";
-String prev_message = "";
-
 int photoValue = 0;
-int sensorValue = 0;
+int vibrationValue = 0;
+int vibrationCount = 0;
+
+boolean isWalkingPrev = false;
+boolean isWalkingCurr = false;
+
+int WALKING_CONSTANT = 35;
+
+int timer = 0;
+
+void checkWalking()
+{
+  isWalkingPrev = isWalkingCurr;
+  
+  xValuePrev = xValueCurr;
+  yValuePrev = yValueCurr;
+  zValuePrev = zValueCurr;
+  
+  xValueCurr = analogRead(ACCELERATION_X);
+  yValueCurr = analogRead(ACCELERATION_Y);
+  zValueCurr = analogRead(ACCELERATION_Z);
+  
+  dx = abs(xValueCurr - xValuePrev);
+  dy = abs(yValueCurr - yValuePrev);
+  dz = abs(zValueCurr - zValuePrev);
+  
+  // Serial.println(dx + dy + dz);
+  if (dx + dy + dz > WALKING_CONSTANT)
+  {
+    // Serial.println("true");
+    isWalkingCurr = true;
+  }
+  else
+  {
+    // Serial.println("false");
+    isWalkingCurr = false;
+  }
+}
 
 void setup()
 {
   Serial.begin(9600);
+  
+  // Timer3.attachInterrupt(checkWalking);
+  // Timer3.start(500000);
 }
 
 void loop()
 {
+  timer %= 30;
+  
+  if (timer == 0)
+  {
+    checkWalking();
+  }
+  
+  // check the photo interrupter's status
   photoValue = analogRead(SENSOR_PHOTO);
   if ( photoValue > 1000 )
   {
     // bag is open
-    message="";
-    sensorValue = analogRead(SENSOR_VIBRATION);
-    // serialRotationValue = constrain(serialRotationValue,0,100);
-  
-    message = String(sensorValue);
-    Serial.println("vibration : " + message);
-  }
-  delay(1000);  
+    // Serial.println("check");
+    if (isWalkingCurr == true && isWalkingPrev == false)
+    {
+      // open walking =)
+      // signal to processing by bluetooth
+      // Serial.println("****************");
+      Serial.write('O');
+      // Serial.println("****************");
+    }
+  }  
 }
--- ARDUINO/isClosed/isClosed.pde
+++ ARDUINO/isClosed/isClosed.pde
@@ -1,1 +1,51 @@
+import processing.serial.*;     // import the Processing serial library
+Serial bluetoothPort;
+char val;
+boolean alert;
+boolean alertDrawFlag;
+int alertCount;
 
+void alertDraw()
+{
+  if (alert == false)
+  {
+    return;
+  }
+  
+  fill(240, 240, 240);
+  textSize(36);
+  text("Close your bag", 170, 310);
+  ++alertCount;
+  if (alertCount > 120)
+  {
+    alertCount = 0;
+    alert = false;
+  }
+}
+
+void setup()
+{
+  size(600, 600);
+  frameRate(60);
+  noStroke();
+  
+  alert = false;
+  alertDrawFlag = false;
+  alertCount = 0;
+  bluetoothPort = new Serial(this, "/dev/tty.AdafruitEZ-Link3f5a-SPP", 9600);
+
+  textFont(createFont("NanumGothic-48", 48));
+}
+
+void draw()
+{
+  background(20, 20, 20);
+  val = (char)bluetoothPort.read();
+  
+  if (val == 'O')
+  {
+    alert = true;
+  }
+  
+  alertDraw();
+}
