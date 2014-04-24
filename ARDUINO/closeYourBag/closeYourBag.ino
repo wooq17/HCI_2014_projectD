dd84870b8a5a30cd8507645de207028f0aae01e1
NHNNEXT_1 Choi Gyeongwook <wooq@nhnnext.org>
2014-04-24 12:45:24 +0900

    photo interrupt with vibration sensor

--- ARDUINO/closeYourBag/closeYourBag.ino
+++ ARDUINO/closeYourBag/closeYourBag.ino
@@ -1,1 +1,29 @@
+int SENSOR_VIBRATION = A0;
+int SENSOR_PHOTO = A3;
 
+String message = "";
+String prev_message = "";
+
+int photoValue = 0;
+int sensorValue = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  photoValue = analogRead(SENSOR_PHOTO);
+  if ( photoValue > 1000 )
+  {
+    // bag is open
+    message="";
+    sensorValue = analogRead(SENSOR_VIBRATION);
+    // serialRotationValue = constrain(serialRotationValue,0,100);
+  
+    message = String(sensorValue);
+    Serial.println("vibration : " + message);
+  }
+  delay(1000);  
+}
